baf9f40981e627adae4dd3731662ec08ad3ecc7b
Implex1v <Implex1v@gmail.com>
2023-02-27 20:33:46 +0100

    feat(youtube-worker): add consumer struct for consumers

---
+++ youtube-worker/messaging/consumer.go
@@ -0,0 +1,40 @@
+package messaging
+
+import (
+	"github.com/segmentio/kafka-go"
+	"time"
+)
+
+type KafkaConsumer struct {
+	BrokerUrl string
+	Topic     string
+	GroupId   string
+	Name      string
+	consume   func(message *KafkaMessage)
+}
+
+type KafkaMessage struct {
+	Topic         string
+	Partition     int
+	Offset        int64
+	HighWaterMark int64
+	Key           []byte
+	Value         []byte
+	Headers       []kafka.Header
+	WriterData    interface{}
+	Time          time.Time
+}
+
+func of(message kafka.Message) *KafkaMessage {
+	return &KafkaMessage{
+		Topic:         message.Topic,
+		Partition:     message.Partition,
+		Offset:        message.Offset,
+		HighWaterMark: message.HighWaterMark,
+		Key:           message.Key,
+		Value:         message.Value,
+		Headers:       message.Headers,
+		WriterData:    message.WriterData,
+		Time:          message.Time,
+	}
+}
--- youtube-worker/messaging/consumers.go
+++ youtube-worker/messaging/consumers.go
@@ -12,43 +12,76 @@ import (
 
 // see: https://github.com/segmentio/kafka-go#reader-
 
-func NewConsumer(l *zap.SugaredLogger, lc fx.Lifecycle, metrics *metrics.AppMetrics) {
-	r := kafka.NewReader(kafka.ReaderConfig{
-		Brokers: []string{"localhost:19092"},
-		Topic:   "youtube.poll",
-		GroupID: "youtube-worker",
-	})
-
-	lc.Append(fx.Hook{
-		OnStart: func(ctx context.Context) error {
-			go func() {
-				for {
-					m, err := r.ReadMessage(context.Background())
-					before := time.Now().Local()
-
-					if err != nil {
-						labels := prom.Labels{"topic": "youtube.poll", "consumer_group": "youtube-worker", "success": "false"}
-						metrics.
-							KafkaConsumeSum.
-							MetricCollector.(*prom.SummaryVec).With(labels).Observe(time.Now().Local().Sub(before).Seconds())
-						l.Errorln("failed to consume message", err)
-						break
-					}
-
-					l.Infof("message at topic/partition/offset %v/%v/%v: %s = %s\n", m.Topic, m.Partition, m.Offset, string(m.Key), string(m.Value))
-					labels := prom.Labels{"topic": "youtube.poll", "consumer_group": "youtube-worker", "success": "true"}
-					metrics.
-						KafkaConsumeSum.
-						MetricCollector.(*prom.SummaryVec).With(labels).Observe(time.Now().Local().Sub(before).Seconds())
-				}
-			}()
-			return nil
+func NewYoutubePollConsumer() KafkaConsumer {
+	return KafkaConsumer{
+		BrokerUrl: "localhost:19092",
+		Topic:     "youtube.poll",
+		GroupId:   "youtube-worker",
+		Name:      "youtube-poll-consumer",
+		consume: func(message *KafkaMessage) {
+			println(string(message.Value))
 		},
-		OnStop: func(ctx context.Context) error {
-			return r.Close()
-		},
-	})
-
+	}
 }
 
-var consumerModule = fx.Module("consumers", fx.Invoke(NewConsumer))
+func RegisterConsumers(consumers []KafkaConsumer, l *zap.SugaredLogger, lc fx.Lifecycle, metrics *metrics.AppMetrics) {
+	for _, consumer := range consumers {
+		l.Debugf("Registering consumer '%v' for topic '%v'", consumer.Name, consumer.Topic)
+
+		r := kafka.NewReader(kafka.ReaderConfig{
+			Brokers: []string{consumer.BrokerUrl},
+			Topic:   consumer.Topic,
+			GroupID: consumer.GroupId,
+		})
+
+		lc.Append(fx.Hook{
+			OnStart: func(ctx context.Context) error {
+				go func() {
+					for {
+						m, err := r.ReadMessage(context.Background())
+						go func() {
+							before := time.Now().Local()
+
+							if err != nil {
+								labels := prom.Labels{
+									"topic":          consumer.Topic,
+									"consumer_group": consumer.GroupId,
+									"success":        "false",
+								}
+								metrics.
+									KafkaConsumeSum.
+									MetricCollector.(*prom.SummaryVec).With(labels).Observe(time.Now().Local().Sub(before).Seconds())
+								l.Errorln("failed to consume message", err)
+								return
+							}
+
+							consumer.consume(of(m))
+							labels := prom.Labels{
+								"topic":          consumer.Topic,
+								"consumer_group": consumer.GroupId,
+								"success":        "true",
+							}
+							metrics.
+								KafkaConsumeSum.
+								MetricCollector.(*prom.SummaryVec).With(labels).Observe(time.Now().Local().Sub(before).Seconds())
+						}()
+					}
+				}()
+				return nil
+			},
+			OnStop: func(ctx context.Context) error {
+				return r.Close()
+			},
+		})
+	}
+}
+
+var consumerModule = fx.Module(
+	"consumers",
+	fx.Provide(
+		fx.Annotate(NewYoutubePollConsumer, fx.ResultTags(`group:"consumers"`)),
+	),
+	fx.Invoke(
+		fx.Annotate(RegisterConsumers, fx.ParamTags(`group:"consumers"`)),
+	),
+)
